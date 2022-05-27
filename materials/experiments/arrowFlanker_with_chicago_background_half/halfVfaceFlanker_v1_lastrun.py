65a1d9bd3701cbe5c923173ada45285682eef7e6
Kianoosh Hosseini <31733665+kianooshhosseini@users.noreply.github.com>
2022-05-27 16:11:35 -0400

    changes

--- materials/experiments/arrowFlanker_with_chicago_background_half/halfVfaceFlanker_v1_lastrun.py
+++ materials/experiments/arrowFlanker_with_chicago_background_half/halfVfaceFlanker_v1_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2022.1.3),
-    on May 27, 2022, at 15:13
+    on May 27, 2022, at 16:09
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
